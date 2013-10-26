a55698122b735e1daf8b3e1f236536022805eec1
qqc <qqc@dev.null>
2013-10-26 17:34:16 +0100

    Added the ability to manually perform a WiFi scan and display the results in the message log.

--- src/ucl/hackathon/ssidchat/WifiAPActivity.java
+++ src/ucl/hackathon/ssidchat/WifiAPActivity.java
@@ -1,13 +1,18 @@
 package ucl.hackathon.ssidchat;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class WifiAPActivity extends Activity {
@@ -18,7 +23,10 @@ public class WifiAPActivity extends Activity {
     private WifiManager wifi;
     private static Button mBtnWifiToggle;
     private Button mBtnChangeSSID;
+    private Button mBtnScan;
     private EditText mSSIDInput;
+    private ListView mMessageLog;
+    private ArrayAdapter<String> mLogAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +35,11 @@ public class WifiAPActivity extends Activity {
 
         mBtnWifiToggle = (Button)findViewById(R.id.btn_wifitoggle);
         mBtnChangeSSID = (Button)findViewById(R.id.btn_changessid);
+        mBtnScan = (Button)findViewById(R.id.btn_scan);
         mSSIDInput = (EditText)findViewById(R.id.txt_ssidinput);
+        mMessageLog = (ListView)findViewById(R.id.messagelog);
+        mLogAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        mMessageLog.setAdapter(mLogAdapter);
 
         wifiAp = new WifiAP();
         wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
@@ -47,6 +59,19 @@ public class WifiAPActivity extends Activity {
 				wifiAp.resetWiFiAP(wifi, WifiAPActivity.this);
 			}
 		});
+        
+        mBtnScan.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				wifi.startScan();
+				List<ScanResult> scanResults = wifi.getScanResults();
+				mLogAdapter.clear();
+				for(ScanResult result : scanResults)
+				{
+					mLogAdapter.add(result.SSID + result.BSSID);
+				}
+			}
+		}); 
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_DIM_BEHIND);       
     }
