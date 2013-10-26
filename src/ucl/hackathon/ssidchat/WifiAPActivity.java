01b31baa67bd9d0e8520ddd9f10e2d906ec6f457
qqc <qqc@dev.null>
2013-10-26 19:25:52 +0100

    Automatic WiFi scanning, messages pulled automatically

--- src/ucl/hackathon/ssidchat/WifiAPActivity.java
+++ src/ucl/hackathon/ssidchat/WifiAPActivity.java
@@ -3,9 +3,13 @@ package ucl.hackathon.ssidchat;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,10 +28,10 @@ public class WifiAPActivity extends Activity {
     private WifiManager wifi;
     private static Button mBtnWifiToggle;
     private Button mBtnChangeSSID;
-    private Button mBtnScan;
     private EditText mSSIDInput;
     private ListView mMessageLog;
     private ArrayAdapter<String> mLogAdapter;
+    private String mPrevMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +40,30 @@ public class WifiAPActivity extends Activity {
 
         mBtnWifiToggle = (Button)findViewById(R.id.btn_wifitoggle);
         mBtnChangeSSID = (Button)findViewById(R.id.btn_changessid);
-        mBtnScan = (Button)findViewById(R.id.btn_scan);
         mSSIDInput = (EditText)findViewById(R.id.txt_ssidinput);
         mMessageLog = (ListView)findViewById(R.id.messagelog);
         mLogAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         mMessageLog.setAdapter(mLogAdapter);
+        mPrevMsg = "";
+        
+        IntentFilter i = new IntentFilter(); 
+        i.addAction (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION); 
+        registerReceiver(new BroadcastReceiver(){ 
+              public void onReceive(Context c, Intent i) {
+            	// Code to execute when SCAN_RESULTS_AVAILABLE_ACTION event occurs 
+            	  WifiManager w = (WifiManager) c.getSystemService(Context.WIFI_SERVICE); 
+            	  scanResultsHandler(w.getScanResults()); // your method to handle Scan results
+            	  w.startScan();
+              }
+        }, i );
 
         wifiAp = new WifiAP();
         wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        WifiLock wifiLock = wifi.createWifiLock(WifiManager.WIFI_MODE_SCAN_ONLY , "MyWifiLock");
+        if(!wifiLock.isHeld()){
+            wifiLock.acquire();
+        }
+        wifi.startScan();
 
         mBtnWifiToggle.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -61,27 +81,6 @@ public class WifiAPActivity extends Activity {
 				wifiAp.resetWiFiAP(wifi, WifiAPActivity.this);
 			}
 		});
-        
-        mBtnScan.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				wifi.startScan();
-				List<ScanResult> scanResults = wifi.getScanResults();
-				if(null == scanResults)
-				{
-					Log.d("WifiAPActivity", "getScanResults() returned null");
-					return;
-				}
-				
-				for(ScanResult result : scanResults)
-				{
-					if(result.SSID.charAt(0) == '%')
-					{
-						mLogAdapter.add(result.SSID.substring(1));
-					}
-				}
-			}
-		}); 
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|WindowManager.LayoutParams.FLAG_DIM_BEHIND);       
     }
@@ -108,6 +107,29 @@ public class WifiAPActivity extends Activity {
             wasAPEnabled = false;
         }
         updateStatusDisplay();
+    }
+    
+    public void scanResultsHandler(List<ScanResult> scanResults)
+    {
+    	Log.d("WifiAPActivity", "SCAN RESULTS RETURNED");
+    	if(null == scanResults)
+		{
+			Log.d("WifiAPActivity", "getScanResults() returned null");
+			return;
+		}
+		
+		for(ScanResult result : scanResults)
+		{
+			if(result.SSID.charAt(0) == '%')
+			{
+				String msg = result.SSID.substring(1);
+				if(mPrevMsg.equals(msg) == false)
+				{
+					mLogAdapter.add(msg);
+				}
+				mPrevMsg = msg;
+			}
+		}
     }
 
     public static void updateStatusDisplay() {
